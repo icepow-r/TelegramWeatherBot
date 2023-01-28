2cc44d718a5b6ea76d3c0df9bfdb26bc37b3d0e2
Илья Барабанов <icepower@mail.ee>
2023-01-28 18:30:15 +0300

    Add project files.

---
+++ TelegramWeatherBot/Program.cs
@@ -0,0 +1,84 @@
+﻿using Newtonsoft.Json;
+using System.Globalization;
+using System.Text;
+using TelegramWeatherBot;
+
+const string telegramToken = "token";
+const string weatherKey = "key";
+const string offsetFile = "offset.txt";
+
+if (!File.Exists(offsetFile))
+{
+    File.Create(offsetFile).Close();
+    File.WriteAllText(offsetFile, "0");
+}
+
+var client = new HttpClient();
+var offset = int.Parse(File.ReadAllText(offsetFile));
+
+while (true)
+{
+    var jsonResponse = client.GetAsync($"https://api.telegram.org/bot{telegramToken}/getUpdates").Result;
+    var tgUpdates = JsonConvert.DeserializeObject<UpdatesResponse>(jsonResponse.Content.ReadAsStringAsync().Result);
+
+    if (!jsonResponse.IsSuccessStatusCode || tgUpdates == null)
+    {
+        Console.WriteLine(DateTime.Now.ToString(CultureInfo.InvariantCulture) + ": Telegram error");
+    }
+    else
+    {
+        for (var i = offset; i < tgUpdates.Result.Length; i++)
+        {
+            var update = tgUpdates.Result[i];
+            var city = update.Message.Text;
+            jsonResponse = client.GetAsync($"http://api.openweathermap.org/data/2.5/weather?q={city}&APPID={weatherKey}&units=metric&lang=ru").Result;
+            var weather = JsonConvert.DeserializeObject<NowWeatherModel>(jsonResponse.Content.ReadAsStringAsync().Result);
+
+            if (!jsonResponse.IsSuccessStatusCode || weather == null)
+            {
+                Console.WriteLine(DateTime.Now.ToString(CultureInfo.InvariantCulture) + ": Weather error");
+            }
+            else
+            {
+                await client.GetAsync($"https://api.telegram.org" +
+                                $"/bot{telegramToken}" +
+                                $"/sendMessage" +
+                                $"?chat_id={update.Message.Chat.Id}&text={BuildMessage(weather)}");
+            }
+        }
+
+        if (offset != tgUpdates.Result.Length)
+        {
+            offset = tgUpdates.Result.Length;
+            File.WriteAllText(offsetFile, offset.ToString());
+        }
+
+    }
+    Thread.Sleep(2000);
+}
+
+static string BuildMessage(NowWeatherModel weather)
+{
+    var message = new StringBuilder();
+
+    var windDirection = weather.Wind.Deg switch
+    {
+        >= 0 and < 15 or >= 345 and < 360 => "с",
+        >= 15 and < 75 => "св",
+        >= 75 and < 105 => "в",
+        >= 105 and < 165 => "юв",
+        >= 165 and < 195 => "ю",
+        >= 195 and < 255 => "юз",
+        >= 255 and < 285 => "з",
+        >= 285 and < 345 => "сз",
+        _ => string.Empty
+    };
+
+    message.AppendLine("Показывается погода для города: " + weather.Name);
+    message.AppendLine($"Температура: {Math.Round(weather.Main.Temp):+#;-#;+0}°C, {weather.Weather[0].Description}");
+    message.AppendLine($"Ощущается как: {Math.Round(weather.Main.FeelsLike):+#;-#;+0}°C");
+    message.AppendLine($"Скорость ветра: {weather.Wind.Speed:N1} м/с, направление: {windDirection}");
+    message.AppendLine($"Влажность: {weather.Main.Humidity}%, давление {weather.Main.Pressure} мм рт. ст.");
+
+    return message.ToString();
+}
---
+++ TelegramWeatherBot/UpdatesResponse.cs
@@ -0,0 +1,74 @@
+﻿using Newtonsoft.Json;
+
+namespace TelegramWeatherBot
+{
+    internal class UpdatesResponse
+    {
+        public bool Ok { get; set; }
+
+        public Result[] Result { get; set; }
+    }
+
+    public class Result
+    {
+        [JsonProperty ("update_id")]
+        public int UpdateId { get; set; }
+
+        public Message Message { get; set; }
+    }
+
+    public class Message
+    {
+        [JsonProperty("message_id")]
+        public int MessageId { get; set; }
+
+        public From From { get; set; }
+
+        public Chat Chat { get; set; }
+
+        public int Date { get; set; }
+
+        public string Text { get; set; }
+
+        public Entity[] Entities { get; set; }
+    }
+
+    public class From
+    {
+        public int Id { get; set; }
+
+        [JsonProperty("is_bot")]
+        public bool IsBot { get; set; }
+
+        [JsonProperty("first_name")]
+        public string FirstName { get; set; }
+
+        public string Username { get; set; }
+
+        [JsonProperty("language_code")]
+        public string LanguageCode { get; set; }
+    }
+
+    public class Chat
+    {
+        public int Id { get; set; }
+
+        [JsonProperty("message_id")]
+        public string FirstName { get; set; }
+
+        public string Username { get; set; }
+
+        public string Type { get; set; }
+    }
+
+    public class Entity
+    {
+        public int Offset { get; set; }
+
+        public int Length { get; set; }
+
+        public string Type { get; set; }
+
+    }
+
+}
